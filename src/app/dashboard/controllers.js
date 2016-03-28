e08347f9d638c6de07d2f777143fa3d713a2a091
Bram Borggreve <borggreve@gmail.com>
2016-03-27 23:03:17 -0500

    Remove obsolete controller from dashboard module

--- src/app/dashboard/controllers.js
+++ src/app/dashboard/controllers.js
@@ -1,13 +1,11 @@
 import angular from 'angular'
 
 import './controllers/alerts'
-import './controllers/master'
 
 const MODULE_NAME = 'app.dashboard.controllers'
 
 angular.module(MODULE_NAME, [
   'app.dashboard.controllers.alerts',
-  'app.dashboard.controllers.master'
 ])
 
 export default MODULE_NAME
--- src/app/dashboard/index.js
+++ src/app/dashboard/index.js
@@ -1,13 +1,11 @@
 import angular from 'angular'
 
-import './components'
 import './controllers'
 import './routes'
 
 const MODULE_NAME = 'app.dashboard'
 
 angular.module(MODULE_NAME, [
-  'app.dashboard.components',
   'app.dashboard.controllers',
   'app.dashboard.routes'
 ])
